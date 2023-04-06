e6a6fb00b3d12543ba6adeb2ee86c73dd063cc8f
Dong <yonghui.dong@gmail.com>
2023-04-06 15:14:07 +0300

    add getRefPeaks function

---
+++ R/fct_getRefPeaks.R
@@ -0,0 +1,13 @@
+#' @title getRefPeaks
+#' @description Get reference peaks from mean spectrum
+#' @param meanSpec An object of class 'MSImagingSummary' obtained from summarizeFeatures function.
+#' @return The return value, if any, from executing the function.
+#' @noRd
+
+getRefPeaks <- function(meanSpec, method = "mad", SNR = 10, tolerance = 10, freq.min = 0.01, workers = 1){
+  meanSpec |>
+    Cardinal::peakPick(method = method, SNR = SNR) |>
+    Cardinal::peakAlign(ref = "mean", tolerance = tolerance, units = "ppm") |>
+    Cardinal::peakFilter(freq.min = freq.min) |>
+    Cardinal::process(BPPARAM = SnowParam(workers = workers, progressbar = T))
+}

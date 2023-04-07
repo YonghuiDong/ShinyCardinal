82ef904bb0b216e9bd058d9a909a14964da51aae
Dong <yonghui.dong@gmail.com>
2023-04-07 21:05:38 +0300

    Update app_server.R

--- R/app_server.R
+++ R/app_server.R
@@ -8,8 +8,10 @@ app_server <- function(input, output, session) {
   options(shiny.maxRequestSize = 10000000 * 1024^2) ## file size limit
   global <- reactiveValues(
     msiData = NULL,
-    meanSpec = NULL
+    meanSpec = NULL,
+    processedMSIData = NULL
   )
   # Your application server logic
   mod_uploadData_server("uploadData_1", global = global)
+  mod_viewData_server("viewData_1", global = global)
 }

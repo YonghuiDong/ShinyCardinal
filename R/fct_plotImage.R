88ae58272dd493d74a12a86a3a261b3b43cf4ff8
Dong <yonghui.dong@gmail.com>
2023-04-20 12:41:25 +0300

    add examples

--- R/fct_plotImage.R
+++ R/fct_plotImage.R
@@ -1,12 +1,23 @@
-#' plotImage
-#'
-#' @description A fct function
-#' @return The return value, if any, from executing the function.
-#'
+#' @title Plot MSI images.
+#' @description Plot MSI images. See image() in Cardinal package.
+#' @param msiData MSI dataset, an object of class 'MSContinuousImagingExperiment'.
+#' @param mz m/z value, can be a vector.
+#' @param smooth.image smoothing method.
+#' @param plusminus mass window.
+#' @param colorscale colorscale.
+#' @param normalize.image Normalization function to be applied to each image.
+#' @param contrast.enhance Contrast enhancement function to be applied to each image.
+#' @param superpose Should different feature images superposed on the same plot?
+#' @return MSI image
 #' @noRd
+#' @examples
+#' library(Cardinal)
+#' set.seed(2020)
+#' mse <- simulateImage(preset = 1, npeaks = 10, nruns = 1, baseline = 1)
+#' plotImage(msiData = mse, mz = 426.529)
 
 
-plotImage <- function(msiData, mz, smooth.image = "none", plusminus = 0.03,
+plotImage <- function(msiData, mz, smooth.image = "none", plusminus = 0.003,
                       colorscale = "cividis", normalize.image = "linear",
                       contrast.enhance = "suppression", superpose = FALSE){
 

a8b7a91bdca3e2432fee42b938b131ce67f4fc8b
Dong <yonghui.dong@gmail.com>
2023-04-20 12:16:24 +0300

    Update fct_colocAnalysis.R

--- R/fct_colocAnalysis.R
+++ R/fct_colocAnalysis.R
@@ -3,7 +3,7 @@
 #' @param msiData MSI dataset, an object of class 'MSContinuousImagingExperiment'.
 #' @param precursor m/z value of the precursor ion used for colocalization analysis.
 #' @param nth every nth pixel will be used for colocalization analysis. This could improve the speed.
-#' @param worker number of workers used for parallel computation.
+#' @param workers number of workers used for parallel computation.
 #' @return a dataframe
 #' @noRd
 #' @examples
@@ -12,12 +12,12 @@
 #' mse <- simulateImage(preset = 1, npeaks = 10, nruns = 1, baseline = 1)
 #' colocAnalysis(msiData = mse, precursor = 773.4709, nth = 1)
 
-colocAnalysis <- function(msiData, precursor, nth = 1,  worker = 1){
+colocAnalysis <- function(msiData, precursor, nth = 1,  workers = 1){
   #(1) Get colocalized features ------------------------------------------------
   msiData[, seq(1, max(Cardinal::pixels(msiData)), by = nth)] |>
     Cardinal::colocalized(object = _,
                           mz = precursor,
                           n = 100,
-                          BPPARAM = BiocParallel::SnowParam(workers = worker, progressbar = T)
+                          BPPARAM = BiocParallel::SnowParam(workers = workers, progressbar = T)
                           )
 }

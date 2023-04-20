84e4f76a8012784a0eb1277d16ab5031033d8fa7
Dong <yonghui.dong@gmail.com>
2023-04-20 12:04:34 +0300

    add examples for the function

--- R/fct_getMeanSpec.R
+++ R/fct_getMeanSpec.R
@@ -1,7 +1,16 @@
-#' @title getMeanSpec
-#' @description A fct function
-#' @return The return value, if any, from executing the function.
+#' @title Calculate mean spectrum
+#' @description Calculate mean spectrum of MSI data.
+#' @param msiData MSI dataset, an object of class 'MSContinuousImagingExperiment'.
+#' @param worker number of workers used for parallel computation.
+#' @return an object of class 'MSContinuousImagingExperiment' containing the mean features and 1 pixel.
 #' @noRd
+#' @examples
+#' @examples
+#' library(Cardinal)
+#' set.seed(2020)
+#' mse <- simulateImage(preset = 1, npeaks = 10, nruns = 1, baseline = 1)
+#' getMeanSpec(msiData = mse)
+
 
 getMeanSpec <- function(msiData, worker = 1){
   Cardinal::summarizeFeatures(x = msiData,

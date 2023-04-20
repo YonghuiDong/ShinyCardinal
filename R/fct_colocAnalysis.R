479a32ae2895a0e9c62547e782786eda72f4cf9b
Dong <yonghui.dong@gmail.com>
2023-04-20 11:55:33 +0300

    # add examples for the function

--- R/fct_colocAnalysis.R
+++ R/fct_colocAnalysis.R
@@ -2,8 +2,13 @@
 #' @description A fct function
 #' @return The return value, if any, from executing the function.
 #' @noRd
+#' @examples
+#' library(Cardinal)
+#' set.seed(2020)
+#' mse <- simulateImage(preset = 1, npeaks = 10, nruns = 1, baseline = 1)
+#' colocAnalysis(msiData = mse, precursor = 773.4709, nth = 1)
 
-colocAnalysis <- function(msiData, precursor, nth = nth,  worker = 1){
+colocAnalysis <- function(msiData, precursor, nth = 1,  worker = 1){
   #(1) Get colocalized features ------------------------------------------------
   msiData[, seq(1, max(Cardinal::pixels(msiData)), by = nth)] |>
     Cardinal::colocalized(object = _,

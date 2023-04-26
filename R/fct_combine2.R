46d85f1f23bacf60eac707af35b8c49b83c071ce
Dong <yonghui.dong@gmail.com>
2023-04-26 22:48:12 +0300

    #11 prepare ROIs for statistics

---
+++ R/fct_combine2.R
@@ -0,0 +1,49 @@
+#' @title Combine different ROIs subset MSI data
+#' @description Get and combine the ROIs subset MSI data, see Cardinal:: combine.
+#' @param msiData msi data
+#' @param roiList a list of user defined ROIs. The names of ROIs must contain two parts:
+#'  before underscore is ROI identifier and after underscore is the condition for statistics.
+#'  such as ROI1_WT, ROI2_WT, ROI3_Mu, ROI4_Mu, 4 ROIs and 2 conditions, WT and Mu.
+#' @return combied MSI Data.
+#' @noRd
+#' @examples
+#' library(Cardinal)
+#' set.seed(1)
+#' x <- simulateImage(preset=1, nruns=2, npeaks=10, dim=c(2,2))
+#' a_X <- c(TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)
+#' b_Y <- c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,  TRUE, TRUE)
+#' c_Y <- c(FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE)
+#' l <- list(a, b, c)
+#' names(l) <- c("a_X", "b_Y", "c_Y")
+#' Result1 <- combine2(msiData = x, roiList = l)
+#' names(l) <- c("a_X", "b_Y", "c_Y_g") # not valid name for statistics
+#' Result2 <- combine2(msiData = x, roiList = l)
+
+
+combine2 <- function(msiData, roiList){
+
+  #(1) subset msiData based on a list of selected ROIs -------------------------
+  m <- lapply(roiList, function(i) msiData[i])
+  #(2) rename run using ROI names ----------------------------------------------
+  lapply(seq_along(m), function(i) {
+    Cardinal::run(m[[i]]) <<- names(m)[i]
+  })
+  #(3) check if the roiList names are valid for statistics----------------------
+  ## if Valid, a condition will be added in pData for statistics
+  pattern <- "^[^_]+_[^_]+$"
+  namesChek <- all(grepl(pattern, names(roiList)))
+  if(isTRUE(namesChek)){
+    lapply(seq_along(m), function(i) {
+      m[[i]]$condition <<- as.factor(strsplit(names(m)[i], "_")[[1]][2])
+    })
+  }
+
+  Cardinal::combine(m)
+}
+
+
+
+
+input_vector <- c("aa_bv", "n_xx", "cccc", "xx_", "_dd", "d_c_d")
+pattern <- "^[^_]+_[^_]+$"
+valid_inputs <- all(grep(pattern, input_vector))

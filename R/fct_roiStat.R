544a4f8cf54a8e89cbef4fc52e482666949f36b8
Dong <yonghui.dong@gmail.com>
2023-04-27 11:51:43 +0300

    #11 Todo 1&2 done

---
+++ R/fct_roiStat.R
@@ -0,0 +1,40 @@
+#' @title Perform statistics on different ROIs.
+#' @description Perform statistics on different ROIs.
+#' @param roiMSIData Combined ROI MSI data. The output from combine2 function.
+#' @return A data frame, including mean ion intensities of the ROIs and/or
+#' @noRd
+#' @examples
+#' library(Cardinal)
+#' set.seed(1)
+#' x <- simulateImage(preset=1, nruns=2, npeaks=10, dim=c(2,2))
+#' a <- c(TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)
+#' b <- c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,  TRUE, TRUE)
+#' c <- c(FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE)
+#' l <- list(a, b, c)
+#' names(l) <- c("a_X", "b_Y", "c_Y")
+#' Result1 <- combine2(msiData = x, roiList = l)
+#' roiStat(roiMSIData = Result1)
+#'
+
+roiStat <- function(roiMSIData){
+  #(1) Get mean intensity for each ROI MSI data -------------------------------
+  roiMean <- Cardinal::aggregate(x = roiMSIData, FUN= c('mean'), groups = Cardinal::run(roiMSIData), as = 'DataFrame') |>
+    as.data.frame(x = _)
+  #(2) Means test --------------------------------------------------------------
+  numPerLevel <- sapply(levels(roiMSIData$condition), function(sLevel) sum(roiMSIData$condition == sLevel))
+  if(!is.null(roiMSIData$condition) & length(levels(roiMSIData$condition)) >= 2 & all(numPerLevel) >= 2){
+    fit <- Cardinal::meansTest(x = roiMSIData, ~ condition, groups = Cardinal::run(roiMSIData))
+    roiStat <- Cardinal::summary(fit) |>
+      as.data.frame(x = _) |>
+      within(data = _, rm("Feature"))
+  } else{
+    roiStat <- NULL
+  }
+  #(3) Show result -------------------------------------------------------------
+  if(!is.null(roiStat)){
+    cbind(roiMean, roiStat)
+  } else{
+    roiMean
+  }
+
+}

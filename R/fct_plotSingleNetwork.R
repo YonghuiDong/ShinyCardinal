be4cedeaec59195e3dd67db5a97fe6b3a5351478
Dong <yonghui.dong@gmail.com>
2023-05-01 16:53:14 +0300

    #15 function for single feature

---
+++ R/fct_plotSingleNetwork.R
@@ -0,0 +1,40 @@
+#' @title Plot network for a single feature.
+#' @description Plot network for a single feature.
+#' @param PCC pearson correlation coefficient obtained from getPCC function.
+#' @param threshold PCC threshold.
+#' @param labelSize label size.
+#' @return network Plot.
+#' @noRd
+#' @examples
+#' library(Cardinal)
+#' set.seed(2020)
+#' mse <- simulateImage(preset = 1, npeaks = 10, nruns = 1, baseline = 1)
+#' PCC <- getPCC(mse, mz = 493.3798)
+#' plotSingleNetwork(PCC, mz = 493.3797)
+
+
+plotSingleNetwork <- function(PCC, mz, threshold = 0.9, labelSize = 40){
+  #(1) Prepare igraph network object -------------------------------------------
+  PCC <- PCC[PCC$correlation >= threshold,]
+
+  ## In many times, the user input mz is not identical to the real mz in the data.
+  ## I need to use the one from the real data, otherwise, the edges will disappear.
+  mzFrom <- PCC[PCC$correlation == 1.0, ]$mz
+  if(length(mzFrom) > 1){
+    mzFrom <- mzFrom[which.min(abs(mzFrom - mz))]
+  }
+  edges <- cbind.data.frame(from = rep(mzFrom, dim(PCC)[1]),
+                            to = PCC$mz,
+                            weight = PCC$correlation
+                            )
+  nodes <- data.frame(id = PCC$mz)
+  nodes$label <- as.character(PCC$mz)
+  edges$value <- PCC$correlation
+
+  #(2) Plot network ------------------------------------------------------------
+  visNetwork::visNetwork(nodes = nodes, edges = edges, width = "100%") %>%
+    visNetwork::visNodes(font = list(size = labelSize)) %>%
+    visNetwork::visOptions(highlightNearest = TRUE) %>%
+    visNetwork::visInteraction(navigationButtons = TRUE) %>%
+    visNetwork::visIgraphLayout()
+}

a9f7cc0484944f5b0aa711b0b6de5aee82382988
Dong <yonghui.dong@gmail.com>
2023-05-01 15:19:11 +0300

    #15 support both all features and a single feature

--- R/fct_getPCC.R
+++ R/fct_getPCC.R
@@ -1,5 +1,5 @@
 #' @title Calculate Pearson correlation coefficient.
-#' @description Calculate Pearson correlation coefficient.
+#' @description Calculate Pearson correlation coefficient either for all mass features or a single mass feature.
 #' @param msiData MSI data set.
 #' @param nth subset MSI data by selecting every nth pixel.
 #' @param msiRun MSI runs.
@@ -9,15 +9,24 @@
 #' library(Cardinal)
 #' set.seed(2020)
 #' mse <- simulateImage(preset = 1, npeaks = 10, nruns = 1, baseline = 1)
+#' ## all mass features
 #' getPCC(mse)
+#' ## single mass feature
+#' getPCC(mse, mz = 493.3798)
 
-getPCC <- function(msiData, nth = 1, msiRun = "All"){
+getPCC <- function(msiData, mz = NULL, nth = 1, msiRun = "All"){
   if(msiRun != "All"){
     msiData <- msiData[Cardinal::run(msiData) == msiRun]
   }
   nth <- ifelse(nth > max(Cardinal::pixels(msiData)), 1, nth) # make sure nth is smaller than max pixels.
   msiData <- msiData[, seq(1, max(Cardinal::pixels(msiData)), by = nth)]
-  specData <- Cardinal::spectra(msiData)
-  rownames(specData) <- round(Cardinal::mz(msiData), 4)
-  cor(t(specData))
+  if(is.null(mz)){
+    specData <- Cardinal::spectra(msiData)
+    rownames(specData) <- round(Cardinal::mz(msiData), 4)
+    cor(t(specData))
+  } else {
+    Cardinal::colocalized(object = msiData, mz = mz, n = Inf) |>
+      subset(x = _, select = c("mz", "correlation")) |>
+      as.data.frame(x = _)
+  }
 }

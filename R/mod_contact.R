a741db4a23e93abebe8f6f65bcc120ebfe570614
Dong <yonghui.dong@gmail.com>
2023-05-07 16:19:30 +0300

    add contact module

---
+++ R/mod_contact.R
@@ -0,0 +1,34 @@
+#' contact UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+#' @importFrom markdown mark
+mod_contact_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    fluidRow(
+      column(width = 10, includeMarkdown(app_sys("app/www/contact.md")))
+    )
+  )
+}
+
+#' contact Server Functions
+#'
+#' @noRd
+mod_contact_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+
+  })
+}
+
+## To be copied in the UI
+# mod_contact_ui("contact_1")
+
+## To be copied in the server
+# mod_contact_server("contact_1")

b62076cec16f8a5e15ae052869be67ca23c89851
Dong <yonghui.dong@gmail.com>
2023-05-08 21:30:27 +0300

    #26

--- R/fct_plotMeanSpec.R
+++ R/fct_plotMeanSpec.R
@@ -14,19 +14,19 @@
 
 plotMeanSpec <- function(meanSpec, nth = 1){
   data <- data.frame(mz = meanSpec@featureData@mz, mean = Cardinal::iData(meanSpec)[,1])
-
   ## define title
   if(nth == 1){
     title = ""
   } else {
     title = paste("mean spectrum for MSI data subsetted by every", nth, "pixels", sep = " ")
   }
-  plotly::plot_ly(data = data) %>%
-    plotly::add_segments(x = ~ mz,
-                         xend = ~ mz,
-                         y = 0,
-                         yend = ~ mean,
-                         line = list(color = "#03946d")) %>%
+  if(!isTRUE(Cardinal::centroided(meanSpec))){
+    p <- plotly::plot_ly(data, x = ~ mz, y = ~ mean, type = 'scatter', mode = 'lines')
+  } else {
+    p <- plotly::plot_ly(data = data) %>%
+      plotly::add_segments(x = ~ mz, xend = ~ mz, y = 0, yend = ~ mean)
+  }
+  p %>%
     plotly::layout(xaxis = list(title = 'm/z'),
                    yaxis = list(title = 'Intensity'),
                    title = title

c09cfd674613cd4a93b8a00875045db104a4d5dc
Dong <yonghui.dong@gmail.com>
2023-05-10 21:29:18 +0300

    Add option for smoothing and baseline reduction

--- R/fct_processMSIData.R
+++ R/fct_processMSIData.R
@@ -4,10 +4,39 @@
 #' @param red reference peaks
 #' @return The return value, if any, from executing the function.
 #' @noRd
+#' @examples
+#' library(Cardinal)
+#' set.seed(2)
+#' mse <- simulateImage(preset=1, npeaks=10, dim=c(3,3), baseline=3)
+#' ref <- summarizeFeatures(mse, FUN="mean")
+#' processMSIData(msiData = mse, ref = ref, ifSmoothing = TRUE, ifBLReduction = TRUE)
 
-processMSIData <- function(msiData, method = "tic", ref, tolerance = 15, workers = 6){
-  msiData |>
-    Cardinal::normalize(method = method) |>
-    Cardinal::peakBin(ref = Cardinal::mz(ref), tolerance = tolerance, units = "ppm") |>
-    Cardinal::process(BPPARAM = BiocParallel::SnowParam(workers = workers, progressbar = FALSE))
+processMSIData <- function(msiData, normMethod = "tic", ref, ifSmoothing = FALSE, smoothMethod = "gaussian",
+                           ifBLReduction = FALSE, blReductionMethod = "locmin", tolerance = 15, workers = 1){
+  if(!ifSmoothing & !ifBLReduction){
+    msiData |>
+      Cardinal::normalize(method = normMethod) |>
+      Cardinal::peakBin(ref = Cardinal::mz(ref), tolerance = tolerance, units = "ppm") |>
+      Cardinal::process(BPPARAM = BiocParallel::SnowParam(workers = workers, progressbar = FALSE))
+  } else if(ifSmoothing & !ifBLReduction){
+    msiData |>
+      Cardinal::normalize(method = normMethod) |>
+      Cardinal::smoothSignal(method = smoothMethod) |>
+      Cardinal::peakBin(ref = Cardinal::mz(ref), tolerance = tolerance, units = "ppm") |>
+      Cardinal::process(BPPARAM = BiocParallel::SnowParam(workers = workers, progressbar = FALSE))
+  } else if(!ifSmoothing & ifBLReduction){
+    msiData |>
+      Cardinal::normalize(method = normMethod) |>
+      Cardinal::reduceBaseline(method = blReductionMethod) |>
+      Cardinal::peakBin(ref = Cardinal::mz(ref), tolerance = tolerance, units = "ppm") |>
+      Cardinal::process(BPPARAM = BiocParallel::SnowParam(workers = workers, progressbar = FALSE))
+  } else{
+    msiData |>
+      Cardinal::normalize(method = normMethod) |>
+      Cardinal::smoothSignal(method = smoothMethod) |>
+      Cardinal::reduceBaseline(method = blReductionMethod) |>
+      Cardinal::peakBin(ref = Cardinal::mz(ref), tolerance = tolerance, units = "ppm") |>
+      Cardinal::process(BPPARAM = BiocParallel::SnowParam(workers = workers, progressbar = FALSE))
+  }
+
 }

2a90cac3ed92a87a2ed5c438c6c885ddc1c5086c
Dong <yonghui.dong@gmail.com>
2023-05-16 17:02:47 +0300

    #24 change default parameters

--- R/fct_deisotoping.R
+++ R/fct_deisotoping.R
@@ -1,7 +1,6 @@
 #' @title Deisotoping
 #' @description Deisotoping of MSI data.
 #' @param msiData MSI data set.
-#' @param ncomp number of PCA components.
 #' @param tol mass accuracy tolerance, unit in Da.
 #' @param PCC Pearson correlation coefficient threshould (pseudo colocalization).
 #' @return deisotoped MSI data set.
@@ -12,14 +11,14 @@
 #' mse <- simulateImage(preset = 1, npeaks = 10, nruns = 2, baseline = 1)
 #' deisotoping(msiData = mse)
 
-deisotoping <- function(msiData, tol = 0.005, PCC = 0.8) {
+deisotoping <- function(msiData, tol = 0.006, PCC = 0.85) {
   idx_iso = c()
   lb <- 1.003355 - tol
   ub <- 1.003355 + tol
-  ims_mass <- Cardinal::mz(msiData)
-  for (i in 1:length(ims_mass)) {
-    m <- ims_mass[i]
-    idx <- c(which((ims_mass >= (m + lb)) & (ims_mass <= (m + ub))))
+  mzs <- Cardinal::mz(msiData)
+  for (i in 1:length(mzs)) {
+    m <- mzs [i]
+    idx <- c(which((mzs >= (m + lb)) & (mzs <= (m + ub))))
     if(length(idx) > 0){
       idx <- idx[sapply(idx, function(j) cor(t(Cardinal::iData(msiData[i, ])), t(Cardinal::iData(msiData[j, ])))) >= PCC]
     }

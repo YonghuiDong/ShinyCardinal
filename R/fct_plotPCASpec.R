480d91361bf1f3ac34789806ead9fce846a835b3
Dong <yonghui.dong@gmail.com>
2023-05-28 11:39:34 +0300

    #40

--- R/fct_plotPCASpec.R
+++ R/fct_plotPCASpec.R
@@ -9,7 +9,7 @@
 #' set.seed(2020)
 #' mse <- simulateImage(preset = 1, npeaks = 10, nruns = 2, baseline = 1)
 #' pcaResult <- getPCA(msiData = mse, msiRun = "run0")
-#' plotPCASpec(pcaResult = pcaResult, msiRun = "run0", clusters = c(1, 3))
+#' plotPCASpec(pcaResult = pcaResult, msiRun = "run0", clusters = c(3, 1))
 
 plotPCASpec <- function(pcaResult, msiRun, clusters){
   #(1) Prepare the data --------------------------------------------------------
@@ -23,7 +23,7 @@ plotPCASpec <- function(pcaResult, msiRun, clusters){
   plot_list <- vector("list", length = length(clusters))
 
   for(i in seq_along(clusters)){
-    df <- data.frame(mz = pcaLoadings$mz, Loadings = pcaLoadings[, (1+i)])
+    df <- data.frame(mz = pcaLoadings$mz, Loadings = pcaLoadings[, (1 + clusters[i])])
     p <- plotly::plot_ly(data = df) %>%
       plotly::add_segments(x = ~ mz,
                            xend = ~ mz,

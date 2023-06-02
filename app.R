1f1862ab03672794f590cb6de41f2aef3c0f8c88
Dong <yonghui.dong@gmail.com>
2023-06-02 21:49:32 +0300

    add option to deploy to shinyapps.io

---
+++ app.R
@@ -0,0 +1,7 @@
+# Launch the ShinyApp (Do not remove this comment)
+# To deploy, run: rsconnect::deployApp()
+# Or use the blue button on top of this file
+
+#pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE) # use it when I need to deploy
+options( "golem.app.prod" = TRUE)
+ShinyCardinal::run_app() # add parameters here (if any)

16930621652597cce5e5fe7ae833c3c5078f15bd
Dong <yonghui.dong@gmail.com>
2023-06-07 17:50:30 +0300

    Update app.R

--- app.R
+++ app.R
@@ -2,6 +2,6 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-#pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE) # use it when I need to deploy
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE) # use it when I need to deploy
 options( "golem.app.prod" = TRUE)
 ShinyCardinal::run_app() # add parameters here (if any)

575ad2ba25e375501313ef28575c4088507e2ebd
Dong <yonghui.dong@gmail.com>
2023-07-17 20:55:44 +0300

    #61

--- R/mod_identification.R
+++ R/mod_identification.R
@@ -24,52 +24,10 @@ mod_identification_ui <- function(id){
                p("1. Please watch video tutorials about metabolite identification based on accurate mass:",
                  a(href = "https://www.youtube.com/watch?v=vjhJwCZFpN0", shiny::icon("youtube", style = "color:#d17789; font-size:25px;"), target="_blank"))
              )
-      ),
+            ),
 
       #(1) Optional: upload MSI rds Data =======================================
-      column(width = 12, h5("Upload MSI rds Data (Optional)")),
-      column(width = 4,
-             box(
-               width = 12,
-               title = strong("Input Parameters"),
-               status = "primary",
-               solidHeader = TRUE,
-               collapsible = TRUE,
-               collapsed = TRUE,
-               closable = FALSE,
-               p(style = "color:#C70039;", shiny::icon("bell"), strong("Note:")),
-               p(style = "color:#C70039;", "1. This moduel is optional."),
-               p(style = "color:#C70039;", "2. If you're starting directly from this module, you need to upload the rds file."),
-               fileInput(inputId = ns("rdsMSI"),
-                         label = "Please select the rds file.",
-                         multiple = FALSE,
-                         placeholder = "",
-                         accept = c(".rds")
-                         ),
-               actionButton(inputId = ns("loadData"),
-                            label = "Load",
-                            icon = icon("paper-plane"),
-                            style = "color: #fff; background-color: #67ac8e; border-color: #67ac8e"
-                            )
-             )
-      ),
-      #(1.2) Upload MSI rds data result ----------------------------------------
-      column(width = 8,
-             box(
-               width = 12,
-               title = strong("Result"),
-               status = "success",
-               solidHeader = TRUE,
-               collapsible = TRUE,
-               collapsed = TRUE,
-               closable = FALSE,
-               shinycssloaders::withSpinner(
-                 image = 'www/img/cardinal.gif',
-                 shiny::verbatimTextOutput(outputId = ns("infoMSIData"))
-               )
-             )
-      ),
-
+      mod_readRDS_ui(ns("readRDS_1")),
       #(2) Identification ======================================================
       column(width = 12, h6("Metabolite Identification")),
       column(width = 4,
@@ -147,22 +105,9 @@ mod_identification_ui <- function(id){
 mod_identification_server <- function(id, global){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
-
     #(1) Load MSI rds Data =====================================================
-    output$infoMSIData <- shiny::renderPrint({
-      shiny::validate(need(!is.null(input$rdsMSI), message = "rds file not found"))
-      global$processedMSIData <- readRDS(input$rdsMSI$datapath)
-      if(is.null(global$processedMSIData)){
-        cat("MSI data not loaded, please check if your rds file is empty.\n")
-      } else {
-        cat("MSI data loaded successfully!\n")
-        global$processedMSIData
-      }
-    }) |>
-      bindEvent(input$loadData)
-
+    mod_readRDS_server("readRDS_1", global = global)
     #(2) Identification ========================================================
-
     output$idTable <- DT::renderDT(server = FALSE, {
       #(1) Get m/z values ------------------------------------------------------
       if(input$mzValueType == "singleMZ"){
--- R/mod_segmentation.R
+++ R/mod_segmentation.R
@@ -28,49 +28,7 @@ mod_segmentation_ui <- function(id){
            ),
 
     #(1) Optional: upload MSI rds Data =========================================
-    column(width = 12, h5("Upload MSI Data")),
-    column(width = 4,
-           box(
-             width = 12,
-             title = strong("Input Parameters"),
-             status = "primary",
-             solidHeader = TRUE,
-             collapsible = TRUE,
-             collapsed = TRUE,
-             closable = FALSE,
-             p(style = "color:#C70039;", shiny::icon("bell"), strong("Note:")),
-             p(style = "color:#C70039;", "1. This moduel is optional."),
-             p(style = "color:#C70039;", "2. If you're starting directly from this module, you need to upload the rds file."),
-             fileInput(inputId = ns("rdsMSI"),
-                       label = "Please select the rds file.",
-                       multiple = FALSE,
-                       placeholder = "",
-                       accept = c(".rds")
-                       ),
-             actionButton(inputId = ns("loadData"),
-                          label = "Load",
-                          icon = icon("paper-plane"),
-                          style = "color: #fff; background-color: #67ac8e; border-color: #67ac8e"
-                          )
-            )
-           ),
-    #(1.2) Upload MSI rds data result ------------------------------------------
-    column(width = 8,
-           box(
-             width = 12,
-             title = strong("Result"),
-             status = "success",
-             solidHeader = TRUE,
-             collapsible = TRUE,
-             collapsed = TRUE,
-             closable = FALSE,
-             shinycssloaders::withSpinner(
-               image = 'www/img/cardinal.gif',
-               shiny::verbatimTextOutput(outputId = ns("infoMSIData"))
-             )
-            )
-          ),
-
+    mod_readRDS_ui(ns("readRDS_1")),
     #(2) PCA ===================================================================
     column(width = 12, h6("Pricipal Component Analysis (PCA)")),
     column(width = 4,
@@ -310,18 +268,7 @@ mod_segmentation_server <- function(id, global){
     ns <- session$ns
 
     #(1) Load MSI rds Data =====================================================
-    output$infoMSIData <- shiny::renderPrint({
-      shiny::validate(need(!is.null(input$rdsMSI), message = "rds file not found"))
-      global$processedMSIData <- readRDS(input$rdsMSI$datapath)
-      if(is.null(global$processedMSIData)){
-        cat("MSI data not loaded, please check if your rds file is empty.\n")
-      } else {
-        cat("MSI data loaded successfully!\n")
-        global$processedMSIData
-      }
-    }) |>
-      bindEvent(input$loadData)
-
+    mod_readRDS_server("readRDS_1", global = global)
     #(2) PCA ===================================================================
     #(2.1) Update MSI run ------------------------------------------------------
     observeEvent(global$processedMSIData, {
